5fc1a198aa1c7a7f93a4e8cb71544dfc0cef31d4
jagathda <mail.jagath@gmail.com>
2024-10-02 13:48:20 +0300

    Defined ECR repository

--- lib/event_bridge_fargate_cloud_watch_postgre_sql-cdk-stack.ts
+++ lib/event_bridge_fargate_cloud_watch_postgre_sql-cdk-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
 
 export class EventBridgeFargateCloudWatchPostgreSqlCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,6 +17,9 @@ export class EventBridgeFargateCloudWatchPostgreSqlCdkStack extends cdk.Stack {
     const cluster = new ecs.Cluster(this, 'MyCluster', {
       vpc: vpc, // Cluster associated with the VPC
     });
+
+    // Define ECR repository for the ECS task's Docker image
+    const repository = ecr.Repository.fromRepositoryName(this, 'MyRepo', 'message-logger');
 
   }
 }
