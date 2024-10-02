6342af208b2b360c7a8d0fdb53b157d8d9674f81
jagathda <mail.jagath@gmail.com>
2024-10-02 13:45:19 +0300

    Added VPC for ECS and RDS

--- lib/event_bridge_fargate_cloud_watch_postgre_sql-cdk-stack.ts
+++ lib/event_bridge_fargate_cloud_watch_postgre_sql-cdk-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class EventBridgeFargateCloudWatchPostgreSqlCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC for ECS and RDS
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      maxAzs: 2, // 2 availability zones for high availability
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EventBridgeFargateCloudWatchPostgreSqlCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
